50b6121cfd31bec08aa21dbeef5f991d37ad1ef2
saf <safanaqumar123@gmail.com>
2020-09-15 03:34:21 +0500

    spinner work

--- app/src/main/java/com/example/teddyapp/Add_new_asset.java
+++ app/src/main/java/com/example/teddyapp/Add_new_asset.java
@@ -1,5 +1,6 @@
 package com.example.teddyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -10,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -17,20 +19,30 @@ import android.widget.Toast;
 
 import com.example.teddyapp.AssetDatabase.AssetDatabase;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.teddyapp.Signup.regusername;
 
 public class Add_new_asset extends AppCompatActivity  {
-private Spinner statusspinner,Location,type;
-public static TextInputEditText serialno,Assettag,Description,Department,Remark,User;
+private Spinner statusspinner,Location,type,Department, software_Spinner;
+public static TextInputEditText serialno,Assettag,Description,Remark,User;
 private Button Addasset,cancelasset ;
 private Button qrscanner;
-    private static final int REQUEST_CAMERA = 1;
-
-
+List<String> softtypes = new ArrayList<>( );
     DatabaseReference AssetsDatabaseReference;;
 
 
+DatabaseReference reference;
+    private static final int REQUEST_CAMERA = 1;
+
+    int nextid = 0;
 
 
     @Override
@@ -46,14 +58,22 @@ private Button qrscanner;
         serialno = (TextInputEditText) findViewById(R.id.SerialNo);
         Assettag = (TextInputEditText) findViewById(R.id.AssetTag);
         Description = (TextInputEditText) findViewById(R.id.desc);
-        Department = (TextInputEditText) findViewById(R.id.dept);
+        Department = (Spinner) findViewById(R.id.dept);
         User = (TextInputEditText) findViewById(R.id.username);
         Remark = (TextInputEditText) findViewById(R.id.remark);
+        software_Spinner = (Spinner) findViewById(R.id.softwarespinner);
         Addasset = (Button) findViewById(R.id.addasset);
         cancelasset = (Button)findViewById(R.id.cancelasset);
         AssetsDatabaseReference = FirebaseDatabase.getInstance().getReference("assets");
 
-
+//Getting the username
+//        User.setText(Signup.regusername.getText());
+cancelasset.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+});
 
         qrscanner.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,12 +96,68 @@ private Button qrscanner;
         ArrayAdapter<String> spinerstatusdapter = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Status));
         spinerstatusdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         statusspinner.setAdapter(spinerstatusdapter);
-        ArrayAdapter<String> spinerstatusdaptertype = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.type));
+        final List<String> typess = new  ArrayList<>();
+        typess.add("Monitor");
+        typess.add("CPU");
+        typess.add("KEYBOARD");
+        typess.add("HEADSET");
+        typess.add("TV");
+        typess.add("MODEM");
+        typess.add("PROJECTOR");
+        typess.add("MOUSE");
+        typess.add("PHONE");
+        typess.add("PRINTER");
+
+        ArrayAdapter<String> spinerstatusdaptertype = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,typess);
+
         spinerstatusdaptertype.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        type.setAdapter(spinerstatusdaptertype);
-        ArrayAdapter<String> spinerstatusdapterlocation = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Location));
+
+
+                type.setAdapter(spinerstatusdaptertype);
+      ArrayAdapter<String> spinerstatusdapterlocation = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Location));
         spinerstatusdapterlocation.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Location.setAdapter(spinerstatusdapterlocation);
+
+
+        type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                   @Override
+                                                   public void onItemSelected(AdapterView<?> a, View v, int pos, long id) {
+
+                                                       //Harcoding Brands:
+
+                                      if (a.getItemAtPosition(pos).equals("CPU"))
+                                      {
+                                          software_Spinner.setVisibility(View.VISIBLE);
+                                          softtypes.clear();
+                                          softtypes.add("firefox");
+                                          softtypes.add("Microsoft Office");
+                                          softtypes.add("Microsoft dynamics");
+                                          softtypes.add("Windows Explorer");
+                                          softtypes.add("Firefox");
+                                          softtypes.add("Chrome");
+                                          softtypes.add("SAP");
+                                          softtypes.add("FreshDesk");
+                                          softtypes.add("ZenDesk");
+                                          softtypes.add("ZoboDesk");
+                                          softtypes.add("Hub Spot");
+                                          fillspinner();
+                                      }
+
+                                                   }
+
+                                                   @Override
+                                                   public void onNothingSelected(AdapterView<?> a) {}
+                                               }
+
+        );
+
+        ArrayAdapter<String> spinerstatusdapterdepartment = new ArrayAdapter<>(Add_new_asset.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Department));
+        spinerstatusdapterdepartment.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Department.setAdapter(spinerstatusdapterdepartment);
+
+
+
+
         Addasset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,18 +165,32 @@ private Button qrscanner;
                   String asset_tag = Assettag.getText().toString();
                   String typeofasset = type.getSelectedItem().toString();
 
+
                   String description = Description.getText().toString();
-                  String location = Location.getSelectedItem().toString();
-                  String deprt = Department.getText().toString();
+                  String location = Location. getSelectedItem().toString();
+                  String deprt = Department.getSelectedItem().toString();
                   String statusasset= statusspinner.getSelectedItem().toString();
                   String remark = Remark.getText().toString();
                   AssetDatabase assetDatabase = new AssetDatabase(serial_num,asset_tag,typeofasset,description,location,deprt,statusasset,remark);
+
                 AssetsDatabaseReference.child(serial_num).setValue(assetDatabase);
+                  reference = FirebaseDatabase.getInstance().getReference("Data");
+                 reference.child(location).setValue(assetDatabase);
+                  reference.child(serial_num).setValue(assetDatabase);
                 Toast.makeText(Add_new_asset.this,"Added",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Add_new_asset.this,MainActivity.class);
+                startActivity(intent);
 
             }
         });
     }
+ public  void fillspinner()
+ {
 
+     ArrayAdapter<String> adapter_2 = new ArrayAdapter<>(this,
+             android.R.layout.simple_list_item_1,softtypes);
+     adapter_2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+     software_Spinner.setAdapter(adapter_2);
+ }
 
 }
--- app/src/main/java/com/example/teddyapp/Asset_update.java
+++ app/src/main/java/com/example/teddyapp/Asset_update.java
@@ -4,23 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Asset_update extends AppCompatActivity {
 Spinner locspinner;
-
+Button confirm_btn;
+DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asset_update);
 
         getSupportActionBar().setTitle("Asset Update");
-locspinner = (Spinner) findViewById(R.id.Loction);
+       locspinner = (Spinner) findViewById(R.id.Loction);
 
         ArrayAdapter<String> spinerstatusdapterlocation = new ArrayAdapter<>(Asset_update.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Location));
         spinerstatusdapterlocation.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         locspinner.setAdapter(spinerstatusdapterlocation);
+
+       /* confirm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference = FirebaseDatabase.getInstance().getReference().child("Data");
+
+
+            }
+        });
+*/
+
+
+        //reference = FirebaseDatabase.getInstance().getReference().child();
 
     }
 }
